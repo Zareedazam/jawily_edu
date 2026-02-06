0c352daa8d11700f6a84a170aa7f37cfe6dbe4ae
Zareedazam <114278607+Zareedazam@users.noreply.github.com>
2026-02-06 23:57:10 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Page() {
   return (
     <div className="min-h-screen bg-white text-black">
@@ -22,15 +23,27 @@ export default function Page() {
               Call us: +91-75188-38702
             </span>
 
-            <button className="px-4 py-2 rounded-full border border-black/15 hover:border-black/30 text-sm font-semibold">
+       {/* Equip Now → APPLY */}
+            <Link
+              href="/apply"
+              className="px-4 py-2 rounded-full border border-black/15 hover:border-black/30 text-sm font-semibold"
+            >
               Equip Now
-            </button>
+            </Link>
 
-            <button className="px-4 py-2 rounded-full border border-black/15 hover:border-black/30 text-sm font-semibold">
+            {/* Login → LOGIN */}
+            <Link
+              href="/login"
+              className="px-4 py-2 rounded-full border border-black/15 hover:border-black/30 text-sm font-semibold"
+            >
               Login
-            </button>
+            </Link>
 
-            <button className="px-4 py-2 rounded-full bg-black text-white hover:opacity-90 text-sm font-semibold">
+            {/* Apply Now → APPLY */}
+            <Link
+              href="/apply"
+              className="px-4 py-2 rounded-full bg-black text-white hover:opacity-90 text-sm font-semibold"
+            >
               Apply Now
             </button>
           </div>
